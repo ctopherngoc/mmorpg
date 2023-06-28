631a2cc970385f789c810beed2583450765cb1ba
Christopher Nguyen <ctopherngoc@gmail.com>
2023-06-28 12:14:08 -0700

    Standardized function and variable names. Removed unused comments and dead code. Formatting.

--- client/scenes/playerObjects/Player.gd
+++ client/scenes/playerObjects/Player.gd
@@ -15,23 +15,23 @@ onready var timer = get_node("Timer")
 
 # dynamic player variables
 onready var player
-onready var jumpSpeed
+onready var jump_speed
 onready var health
-onready var maxHorizontalSpeed
+onready var max_horizontal_speed
 onready var velocity = Vector2.ZERO
 onready var damage
 
 # static player varaibles
-onready var horizontalAcceleration = 3
+onready var horizontal_acceleration = 3
 onready var knockback_modifier = 0.2
 onready var gravity = 800
 
 # player states
-export var climbing = false
-export var isClimbing = false
-export var noColZone = false
+export var can_climb = false
+export var is_climbing = false
+export var no_collision_zone = false
 onready var direction = "RIGHT"
-onready var tookDamage = false
+onready var took_damage = false
 onready var attacking = false
 onready var received_knockback = false
 onready var player_state
@@ -43,16 +43,14 @@ onready var animation = {
 
 
 func _ready():
-	#Server.fetchPlayerDamage(get_instance_id())
 	timer.set_wait_time(1.5)
 	player = Global.register_player()
 	$Label.text = player.displayname
-	maxHorizontalSpeed = (player.stats.movementSpeed)
+	max_horizontal_speed = (player.stats.movementSpeed)
 	
-	jumpSpeed = (player.stats.jumpSpeed)
+	jump_speed = (player.stats.jumpSpeed)
 # warning-ignore:return_value_discarded
 	$take_damage.connect("area_entered", self, "on_hazard_area_entered")
-	#$take_damage.connect("area_exited", self, "on_hazard_area_exit")
 	get_node( "Head" ).set_flip_h( true )
 	get_node( "Body" ).set_flip_h( true )
 	get_node( "Ears" ).set_flip_h( true )
@@ -60,21 +58,21 @@ func _ready():
 	get_node( "Weapon" ).scale.x = -1
 
 func _physics_process(delta):
-	MovementLoop(delta)
-	DefinePlayerState()
-	updateHPDisplay()
+	movement_loop(delta)
+	define_player_state()
+	update_health_display()
 
-func DefinePlayerState():
+func define_player_state():
 	player_state = {"T": Server.client_clock, "P": get_global_position(), "M": Global.current_map, "A": animation}
 	Server.SendPlayerState(player_state)
 
-func MovementLoop(delta):
+func movement_loop(delta):
 	#take_damage()
-	changeDirection()
-	var moveVector = get_movement_vector()
+	change_direction()
+	var move_vector = get_movement_vector()
 	
 	# change get velocity
-	get_velocity(moveVector, delta)
+	get_velocity(move_vector, delta)
 	# warning-ignore:return_value_discarded
 	move_and_slide(velocity, Vector2.UP)
 	
@@ -84,29 +82,23 @@ func MovementLoop(delta):
 	else:
 		animation["f"] = 0
 	
-	if is_on_floor() or !isClimbing:
+	if is_on_floor() or !is_climbing:
 		velocity = move_and_slide(velocity, Vector2.UP)
-	if isClimbing:
+	if is_climbing:
 		velocity.x = 0
-	
-	#animation["animation_vector"] = velocity.normalized()
-		
+
 	update_animation()
 
 func get_movement_vector():
-	#print(self.global_position)
 	var moveVector = Vector2.ZERO
 	
-	# calculating x vector
-	# allow x-axis jump off ropes or idle on floor
+	# calculating x vector, allow x-axis jump off ropes or idle on floor
 	if (!attacking && is_on_floor()) or (Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left")) and Input.get_action_strength("jump"):
 		moveVector.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * .8
 	else:
 		moveVector.x = 0	
-		
-	# calculating y vector
-	# allow jump off ropes
-	if isClimbing:
+	# calculating y vector, allow jump off ropes
+	if is_climbing:
 		if (Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left")) and Input.get_action_strength("jump"):
 			moveVector.y = -1
 		else:
@@ -116,73 +108,67 @@ func get_movement_vector():
 			moveVector.y = -1
 		else:
 			moveVector.y = 0
-			
 	return moveVector
 
-func get_velocity(moveVector, delta):
-	velocity.x += moveVector.x * horizontalAcceleration
+func get_velocity(move_vector, delta):
+	velocity.x += move_vector.x * horizontal_acceleration
 	# slow down movement
-	if(moveVector.x == 0):
+	if(move_vector.x == 0):
 		# allows forward jumping
 		if(is_on_floor()):
 			velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
-#		else:
-#			velocity.x = lerp(0, velocity.x, pow(2, -1 * delta))
-		
-	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
+
+	velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
 	
-	if climbing:
-		if isClimbing:
+	if can_climb:
+		if is_climbing:
 			velocity.y = 0
 			if Input.is_action_pressed("ui_up"):
 				velocity.y = -100
 			elif Input.is_action_pressed("ui_down"):
 				velocity.y = 100
 				if is_on_floor():
-					isClimbing = false
+					is_climbing = false
 			# jump off rope
 			elif Input.is_action_pressed("jump") && (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")):
-				isClimbing = false
-				velocity.y = moveVector.y * jumpSpeed * .8
-				velocity.x = moveVector.x * 200
-				
+				is_climbing = false
+				velocity.y = move_vector.y * jump_speed * .8
+				velocity.x = move_vector.x * 200
+
 		# can climb but not climbing
 		else:
-#			self.set_collision_layer_bit(0, true)
-#			self.set_collision_mask_bit(0, true)
-
-			# if moving
-			if (moveVector.y < 0 && is_on_floor()):
-					velocity.y = moveVector.y * jumpSpeed
+			#if moving
+			if (move_vector.y < 0 && is_on_floor()):
+					velocity.y = move_vector.y * jump_speed
 			# press up on ladder initiates climbing
 			elif (!is_on_floor() && Input.is_action_pressed("ui_up")) or (is_on_floor() && Input.is_action_pressed("ui_down")):
-					isClimbing = true
+					is_climbing = true
 					self.set_collision_layer_bit(0, false)
 					self.set_collision_mask_bit(0, false)
 					velocity.y = 0
-					velocity.x = 0	
+					velocity.x = 0
 			# over lapping ladder pressing nothing allows gravity
 			else:
 				velocity.y += gravity * delta
 	# not climbable state
 	else:
 		# normal movement
-		if (moveVector.y < 0 && is_on_floor()):
-			velocity.y = moveVector.y * jumpSpeed
+		if (move_vector.y < 0 && is_on_floor()):
+			velocity.y = move_vector.y * jump_speed
 		else:
 			velocity.y += gravity * delta
 			
-	if !climbing:
-		isClimbing = false
+	if !can_climb:
+		is_climbing = false
 
 # attack > jump > walking > takeDamage > standing
 func update_animation():
-	var moveVec = get_movement_vector()
+	var move_vector = get_movement_vector()
 	
 	if(attacking):
 		pass
 
-	elif(Input.is_action_pressed("attack") && !isClimbing):
+	elif(Input.is_action_pressed("attack") && !is_climbing):
 		$AnimationPlayer.playback_speed = 0.75
 		attacking = true
 		$AnimationPlayer.play("stab")
@@ -192,7 +178,7 @@ func update_animation():
 		if(!is_on_floor()):
 			$AnimationPlayer.play("jump")
 
-		elif(moveVec.x != 0):
+		elif(move_vector.x != 0):
 			$AnimationPlayer.play("walk")
 	#	elif(tookDamage):
 	#		$Head.play("hit")
@@ -206,7 +192,7 @@ func update_animation():
 		else:
 			$AnimationPlayer.play("idle")
 
-func changeDirection():
+func change_direction():
 	if Input.is_action_pressed("move_right") && !attacking && !Input.is_action_pressed("move_left"):
 		direction = "RIGHT"
 		get_node( "Head" ).set_flip_h( true )
@@ -235,30 +221,26 @@ func changeDirection():
 func on_hazard_area_entered(area2d):
 	pass
 	#recieve_knockback(area2d.global_position)
-	
 
 # warning-ignore:unused_argument
 func recieve_knockback(damage_source_pos: Vector2):
 	pass
-	
 #	var knockback_direction = damage_source_pos.direction_to(self.global_position)
 #	var knockback = knockback_direction * knockback_modifier *40	
 #	self.global_position += knockback
-
 #######################################################################################
+
 # take damage function
 func take_damage():
-	if !tookDamage:
+	if !took_damage:
 		if $take_damage.get_overlapping_areas().size() > 0:
-			# print("player damage from server %s" % damage)
-			#player.stats.mapValue.fields.health.doubleValue -= damage
 
 			print("touch damage. HP = ", player.stats.mapValue.fields.health.doubleValue)
-			tookDamage = true
-		
+			took_damage = true
+
 			if (player.stats.mapValue.fields.health.doubleValue <= 0):
 				# respawn hp
-				tookDamage = false
+				took_damage = false
 				player.stats.mapValue.fields.health.doubleValue = 25
 				print("died update")
 				Global.update_player(player)
@@ -273,7 +255,7 @@ func take_damage():
 
 func _on_Timer_timeout():
 	timer.stop()
-	tookDamage = false
+	took_damage = false
 	$take_damage.set_monitoring(true) 
 
 # insert iFrame
@@ -284,37 +266,34 @@ func on_hazard_area_exit(_area2d):
 #########################################################################################
 # floor collision while in air
 func _on_Area2D_area_entered(_area):
-	noColZone = true
+	no_collision_zone = true
 	self.set_collision_layer_bit(0, false)
 	self.set_collision_mask_bit(0, false)
 	print("no col")
 
 func _on_Area2D_area_exited(_area):
-	noColZone = false
+	no_collision_zone = false
 	self.set_collision_layer_bit(0, true)
 	self.set_collision_mask_bit(0, true)
 	print("col")
 ##########################################################################################
 # attacking functions
+func set_damage(damage_value):
+	damage = damage_value
+	print("player set damage = %s" % damage)
 
-func setDamage(s_damage):
-	damage = s_damage
-	print("player set damage = %s" % s_damage)
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "stab":
+func _on_AnimationPlayer_animation_finished(animation_name):
+	if animation_name == "stab":
 		attacking = false
 		
 func _unhandled_input(event):
 	if event.is_action_pressed("attack"):
-		Server.SendAttack()
+		Server.send_attack()
 
 func overlappingBodies():
-	#print('inside overlappingbodies')
 	print("area ovlapping: " + str($do_damage.get_overlapping_areas().size()))
 	for body in $do_damage.get_overlapping_areas():
-		#if body.is_in_group('enemy'):
-		print(body)
+		print('player overlapping with: ', body)
 
-func updateHPDisplay():
+func update_health_display():
 	$HP.text = str(player["stats"]["health"])
