6bb7bf68fbbec3b80584b21b3dfb28dd074502be
Christopher Nguyen <ctopherngoc@gmail.com>
2023-06-29 12:35:32 -0700

    Standardized function and variable names. Removed comments and unused code.

--- server/singletons/Global.gd
+++ server/singletons/Global.gd
@@ -1,37 +1,32 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-func storeCharacterInfo(player_id, displayname):
-	var playerContainer = get_node(ServerData.player_location[str(player_id)] + "/%s" % str(player_id))
-	var firebase = Firebase.update_document("characters/%s" % displayname, playerContainer.http, playerContainer.db_info["token"], playerContainer.current_character)
+func store_character_data(player_id, display_name):
+	var player_container = get_node(ServerData.player_location[str(player_id)] + "/%s" % str(player_id))
+	var firebase = Firebase.update_document("characters/%s" % display_name, player_container.http, player_container.db_info["token"], player_container.current_character)
 	yield(firebase, 'completed')
-	print("%s saved after level" % displayname)
+	print("%s saved after level" % display_name)
 
-func NPCAttack(player, monster_stats):
-	#print(player.current_character) 
-	#print(monster_stats)
+func npc_attack(player, monster_stats):
 	var player_stats = player.current_character.stats
-	#print(player_stats)
 	# calculate basic hit mechanic and damage formula
 	
 	# hit mechanic
 	if monster_stats["Accuracy"] >= player_stats["dexterity"] * 1.2:
 		#print("Monster Hit: %s" % player.name)
 		
-		# some random damage formula
-		# since theres no player defense use strength as defense
+		# some random damage formula, since theres no player defense use strength as defense
 		var calculation = monster_stats["Attack"] - player_stats["strength"]
 		if calculation > 1:
-			player.TakeDamage(calculation)
+			player.take_damage(calculation)
 		else:
-			player.TakeDamage(1)	
+			player.take_damage(1)	
 	else:
 		print("Monster potentially miss")
 
 # WIP
 # warning-ignore:unused_argument
-func playerDeath(player_id):
+func player_death(player_id):
 	pass
