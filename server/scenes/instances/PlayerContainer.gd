50fcd49830f758b5bb9afef4f2bbc73a993861b3
Christopher Nguyen <ctopherngoc@gmail.com>
2023-08-11 13:18:46 -0700

    Added passive idle healing

--- server/scenes/instances/PlayerContainer.gd
+++ server/scenes/instances/PlayerContainer.gd
@@ -3,14 +3,17 @@ extends KinematicBody2D
 onready var http = $HTTPRequest
 onready var http2 = $HTTPRequest2
 onready var timer =$Timer
+onready var idle_timer =$idle_timer
 #contains token and id
 var db_info = {}
+var cur_position = Vector2.ZERO
 
 # post firestore convert
 var email = ""
 var characters = []
 var characters_info_list = []
 var damage = 10
+var idle_counter = 0
 
 # contains player data
 ########
@@ -95,3 +98,32 @@ func experience(experience):
 func _on_DamageTimer_timeout():
 	hittable = true
 	$DamageTimer.stop()
+
+func start_idle_timer():
+	$idle_timer.start(1.0)
+	print("idle timer start")
+	
+# regen 5hp every 5 seconds if idle
+func _on_idle_timer_timeout():
+	#print(cur_position, self.position)
+	if self.position != cur_position:
+		cur_position = self.position
+		idle_counter = 0
+	else:
+		# print("same position")
+		if self.is_on_floor():
+			idle_counter += 1
+			if idle_counter == 5:
+				if current_character["stats"]["health"] < current_character["stats"]["maxHealth"]:
+					if current_character["stats"]["maxHealth"] - current_character["stats"]["health"] < 5:
+						var hp_dif = current_character["stats"]["maxHealth"] - current_character["stats"]["health"]
+						current_character["stats"]["health"] += int(hp_dif)
+					else:
+						print("heal 5 hp")
+						current_character["stats"]["health"] += 5
+					get_node("/root/Server").update_player_stats(self)
+				else:
+					pass
+				idle_counter = 0
+		else:
+			idle_counter = 0
