afef7bd0412bd24a7cbb6d41ea5bd10c394fc9fe
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-11-20 14:12:08 -0800

    Update PlayerInfo.gd
    Formatting. Added job and level to UI footer

--- client/scenes/userInerface/PlayerInfo.gd
+++ client/scenes/userInerface/PlayerInfo.gd
@@ -1,10 +1,11 @@
 extends ColorRect
 
+"""
 var health = Global.player["stats"]["health"] 
 var mana = Global.player["stats"]["mana"] 
 var level = Global.player["stats"]["level"] 
 var experience = Global.player["stats"]["experience"]
-
+"""
 # Called when the node enters the scene tree for the first time.
 func _ready():
 # warning-ignore:return_value_discarded
@@ -19,26 +20,29 @@ func _ready():
 	Signals.connect("update_displayname", self, "update_displayname")
 	load_info()
 
-
-
 func update_health():
-	$health/Label.text = str(health)
+	$health/Label.text = str(Global.player["stats"]["health"])
 
 func update_mana():
-	$mana/Label.text = str(mana)
+	$mana/Label.text = str(Global.player["stats"]["mana"])
 	
 func update_level():
-	$level/Label.text = str(level)
+	$level/Label.text = str(Global.player["stats"]["level"])
 	
 # for xp to show up as percentage, maximum xp for level must be stored and transferred
 func update_exp():
-	$exp/Label.text = str(experience)
+	$exp/Label.text = str(Global.player["stats"]["experience"])
 
 func update_displayname():
 	$displayname/Label.text = str(Global.player["displayname"])
+	
+func update_job():
+	$job/Label.text = str(Global.job_dict[str(Global.player["stats"]["job"])])
 	
 func load_info():
 	update_displayname()
 	update_health()
 	update_mana()
 	update_exp()
+	update_level()
+	update_job()
