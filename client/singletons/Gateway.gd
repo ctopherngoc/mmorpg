856f8a29f7ce2fc59abfe7a67cd72419c946c5d5
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-11-23 17:39:25 -0800

    Update Gateway.gd
    Updated login button issue. Restructured multiplayer network to work with same login button

--- client/singletons/Gateway.gd
+++ client/singletons/Gateway.gd
@@ -3,15 +3,21 @@ extends Node
 
 var network = NetworkedMultiplayerENet.new()
 var gateway_api = MultiplayerAPI.new()
-var port = 27
-var ip = "172.17.0.3"
+var port = 2734
+var ip = ""
 var cert = load("res://resources/Certificate/X509_Certificate.crt")
 
 var username
 var password
 
 func _ready():
-	pass
+	network = NetworkedMultiplayerENet.new()
+	gateway_api = MultiplayerAPI.new()
+	network.set_dtls_enabled(true)
+	network.set_dtls_verify_enabled(false)
+	network.set_dtls_certificate(cert)
+	network.connect("connection_failed", self, "_on_connection_failed")
+	network.connect("connection_succeeded", self, "_on_connection_succeeded")
 
 func _process(_delta):
 	if get_custom_multiplayer() == null:
@@ -21,24 +27,23 @@ func _process(_delta):
 	custom_multiplayer.poll()
 
 func connect_to_server(_username, _password):
-	network = NetworkedMultiplayerENet.new()
-	gateway_api = MultiplayerAPI.new()
-	network.set_dtls_enabled(true)
-	network.set_dtls_verify_enabled(false)
-	network.set_dtls_certificate(cert)
 	username = _username
 	password = _password
 	network.create_client(ip, port)
 	set_custom_multiplayer(gateway_api)
 	custom_multiplayer.set_root_node(self)
 	custom_multiplayer.set_network_peer(network)
-	
-	network.connect("connection_failed", self, "_on_connection_failed")
-	network.connect("connection_succeeded", self, "_on_connection_succeeded")
 
 func _on_connection_failed():
 	print("Failed to conect to login server")
 	print("Pop-up server offline")
+	Signals.emit_signal("fail_login")
+	
+	network.close_connection()
+	custom_multiplayer.set_network_peer(null)
+	custom_multiplayer.set_network_peer(null)
+	#network = NetworkedMultiplayerENet.new()
+	#gateway_api = MultiplayerAPI.new()
 	
 
 
