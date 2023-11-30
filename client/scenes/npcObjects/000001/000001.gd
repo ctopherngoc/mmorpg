4726920c27de0740a543ce60ffe8437e6198000c
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-11-29 21:28:34 -0800

    Added first npc
    Added sprite and script for an npc named kebin

--- client/scenes/npcObjects/000001/000001.gd
+++ client/scenes/npcObjects/000001/000001.gd
@@ -1,28 +1,41 @@
 extends Area2D
 var can_interact = false
-const diaglog = false
+const DIALOG = preload("res://scenes/userInerface/Dialog.tscn")
 var clicked = false
+var id = "Kevin"
+var sprite = "res://scenes/npcObjects/000001/sprite/stand1_0.png"
 
 func _physics_process(_delta):
 	$AnimatedSprite.play()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+# warning-ignore:return_value_discarded
+	Signals.connect("dialog_closed", self, "dialog_closed")
+
+func dialog_closed():
+	clicked = false
+	print("dialog should be closed and kevin should be clickable")
 
 func _on_Area2D_mouse_entered():
 	can_interact = true
-	print("can interact")
+	print("can interact with kevin")
 
 
 func _on_Area2D_mouse_exited():
 	can_interact = false
-	print("cannot interact")
+	print("cannot interact with kevin")
 
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	#print(event)
-	if event is InputEventMouseButton:
-		print("clicked")
+	if can_interact:
+		if event is InputEventMouseButton and clicked == false:
+			clicked = true
+			print("popup dialog for npc")
+			var dialog = DIALOG.instance()
+			Global.ui.add_child(dialog)
+			Global.movable = false
+			
 
 func _on_Area2D_area_entered(area):
 	print(area)# Replace with function body.
