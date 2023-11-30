9550f626ef5a4e074750167e461d89019e06868e
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-11-30 15:57:08 -0800

    Update SceneHandler.gd
    Updated print statements. removed comments

--- client/singletons/SceneHandler.gd
+++ client/singletons/SceneHandler.gd
@@ -4,6 +4,7 @@ onready var current_scene_container = preload("res://scenes/userInerface/LoginSc
 onready var current_scene = "menu"
 onready var current_bgm = "menu"
 
+# menu hash map
 onready var menu_scenes = {
 	"mainMenu" : "res://scenes/userInerface/mainMenu.tscn",
 	"register" : "res://scenes/userInerface/RegisterScreen.tscn",
@@ -16,32 +17,23 @@ func _ready():
 	get_tree().change_scene("res://scenes/userInerface/LoginScreen.tscn")
 
 func _process(_delta):
+	# turn on music
 	if Global.bgm.playing == false:
 		Global.bgm.playing = true
 
-
 func change_scene(scene: String):
 # warning-ignore:return_value_discarded
+	# if in menu
 	if scene in menu_scenes.keys():
 		get_tree().change_scene(menu_scenes[scene])
 		if current_bgm != "menu":
 			Global.bgm.set_stream(GameData.bgm_dict["menu"])
 	else:
-		"""
-		GameData.map_dict{mapid: {mapname:fdf, path: res...},}
-		scene string should be map_id: EX: 000001
-		1. update global current map to map_id
-		2. get name path
-		3. update global map_name?
-		4.  change scene
-		"""
+		# if in map
 		Global.current_map = scene
-		# map_id
-		print(Global.current_map, GameData.map_dict[scene]["name"])
+		print(Global.current_map, " ", GameData.map_dict[scene]["name"])
 		
-		"""
-		channge. Possibly add  region to map hash and edit bgm keys to be based off of region
-		"""
+		# if map different bgm
 		if GameData.map_dict[scene]["bgm"] != current_bgm:
 			current_bgm = GameData.map_dict[scene]["bgm"]
 			Global.bgm.set_stream(GameData.bgm_dict[GameData.map_dict[scene]["bgm"]])
