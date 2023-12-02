b31cc9a9e01f77244e261a70c35cbd033fa473f6
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2023-12-01 23:45:45 -0800

    Character Creation Screen
    Made character creation screen with sprites and function. Moved username validation to create scene.

--- client/scenes/userInerface/Create.gd
+++ client/scenes/userInerface/Create.gd
@@ -1,8 +1,62 @@
 extends Node2D
 
+onready var check_button = $Create/UsernameCheck
+onready var create_button = $Create/Create
+onready var username_field = $Create/usernameField
+onready var dictionary = "ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+onready var character_count
+var display_name
+var new_username
+var username_check
+
 onready var dudu = null
 func _ready():
 	pass
 
-func _on_Button_pressed():
-	pass # Replace with function body.
+func _on_create_pressed():
+	create_button.disabled = true
+	if username_check and username_field.text == new_username:
+		if string_validation(new_username):
+			Server.create_character(get_instance_id(), new_username)
+	else:
+		$Create/notification/Label.text = "check username availability"
+		create_button.disabled = false
+		
+func username_check_results(results):
+	if results:
+		username_check = true
+		$Create/notification/Label.text = "username not taken"
+	else:
+		$Create/notification/Label.text = "username taken"
+	
+func _on_UsernameCheck_pressed():
+	check_button.disabled = true
+	var username = username_field.text.strip_edges().strip_escapes()
+	print ("checking username: %s" % username)
+	
+	if string_validation(username):
+		Server.check_usernames(get_instance_id(), username)
+		check_button.disabled = false
+		new_username = username
+	
+func created_character():
+	print("successfully created character")
+	SceneHandler.change_scene("characterSelect")
+
+func string_validation(username):
+	if username.length() < 6:
+		$Create/notification/Label.text = "username must be at least 6 characters"
+		check_button.disabled = false
+		return false
+	for letter in username:
+		if not letter in dictionary:
+			$Create/notification/Label.text = "invalid username"
+			check_button.disabled = false
+			return false
+	for banned_word in GameData.string_validation:
+		if banned_word in username.to_lower():
+			$Create/notification/Label.text = "contains inappropirate words"
+			check_button.disabled = false
+			return false
+	return true
+
