c525a4f044d0d783f13eb0cf7f637ee4765a2e94
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-12-04 20:12:51 -0800

    Remove unused scenes

--- client/scenes/menuObjects/mainMenu.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _on_RegisterButton_pressed() -> void:
-	SceneHandler.change_scene(SceneHandler.scenes["register"])
-
-func _on_LoginButton_pressed() -> void:
-	SceneHandler.change_scene(SceneHandler.scenes["login"])
