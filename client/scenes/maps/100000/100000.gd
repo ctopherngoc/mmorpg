f2bb6b689687c01ef9a38b1b7253489ae3bb1379
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-12-06 13:49:47 -0800

    Updated tilemap and ladder mapobject

--- client/scenes/maps/100000/100000.gd
+++ client/scenes/maps/100000/100000.gd
@@ -48,24 +48,18 @@ func create_player():
 	add_child_below_node(main_player, player_instance)
 	player_instance.global_position = spawn_location
 	main_player = player_instance
-	#register_player(player_instance)
 
-"""
-func on_player_died():
-	print("Player Died")
-	main_player.queue_free()
-	print("Play Respawned")
-	create_player()
-"""
 ############################################
-
-func _on_noCol_body_entered(body):
+"""
+	return
 	if body.is_in_group("player"):
 		body.set_collision_layer_bit(0, false)
 		body.set_collision_mask_bit(0, false)
 
 func _on_noCol_body_exited(body):
+	return
 	print("out of area")
 	if body.is_in_group("player"):
 		body.set_collision_layer_bit(0, true)
 		body.set_collision_mask_bit(0, true)
+"""
