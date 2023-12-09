afbb16ab1ec352a011c83d7aea93c87c5a1625ca
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2023-12-08 23:03:36 -0800

    Update Ladder.gd
    removed client control of climbing bools

--- client/scenes/mapObjects/Ladder.gd
+++ client/scenes/mapObjects/Ladder.gd
@@ -1,18 +1,9 @@
 extends Area2D
 
 func _on_Ladder_area_entered(area):
-	print("over rope")
-	print(area)
 	if area.is_in_group("climber"):
 		var parent = area.get_parent()
-		if parent.can_climb == false:
-			parent.can_climb = true
-			print(parent.can_climb)
-
 
 func _on_Ladder_area_exited(area):
 	if area.is_in_group("climber"):
 		var parent = area.get_parent()
-		if parent.can_climb == true:
-			parent.can_climb = false
-			print(parent.can_climb)
