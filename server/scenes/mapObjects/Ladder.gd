95e7da6967e4776441ceb5bb4a6cc55987e8bbb3
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2023-12-08 23:01:01 -0800

    Update Ladder.gd
    implemented server climbing bool syncing with client

--- server/scenes/mapObjects/Ladder.gd
+++ server/scenes/mapObjects/Ladder.gd
@@ -1,17 +1,16 @@
 extends Area2D
 
 func _on_Ladder_area_entered(area):
-	print("over rope")
 	if area.is_in_group("climber"):
 		var parent = area.get_parent()
 		if parent.can_climb == false:
+			Global.send_climb_data(self.name, 1)
 			parent.can_climb = true
-			print(parent.can_climb)
 
 
 func _on_Ladder_area_exited(area):
 	if area.is_in_group("climber"):
 		var parent = area.get_parent()
 		if parent.can_climb == true:
+			Global.send_climb_data(self.name, 0)
 			parent.can_climb = false
-			print(parent.can_climb)
