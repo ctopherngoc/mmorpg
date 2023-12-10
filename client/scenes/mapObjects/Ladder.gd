57cb7a52aeebcfa36cf3bf619cb21e7c1359b4c0
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2023-12-09 16:54:27 -0800

    Update Ladder.gd
    Added defer warning

--- client/scenes/mapObjects/Ladder.gd
+++ client/scenes/mapObjects/Ladder.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 func _on_Ladder_area_entered(area):
 	if area.is_in_group("climber"):
+# warning-ignore:unused_variable
 		var parent = area.get_parent()
 
 func _on_Ladder_area_exited(area):
 	if area.is_in_group("climber"):
+# warning-ignore:unused_variable
 		var parent = area.get_parent()
