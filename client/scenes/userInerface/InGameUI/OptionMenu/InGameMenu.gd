810aaffcf8114938cab2cc695a46b17a351c20f3
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-12-15 16:47:00 -0800

    Update InGameMenu.gd

--- client/scenes/userInerface/InGameUI/OptionMenu/InGameMenu.gd
+++ client/scenes/userInerface/InGameUI/OptionMenu/InGameMenu.gd
@@ -6,40 +6,41 @@ onready var AnimPlayer = $AnimationPlayer
 onready var OptionMenu = $Options
 onready var MenuMenu = $menu
 
-onready var HowToPlay = $HowToPlay
-
 export(String, FILE, "*.tscn") var NextScene
 var Paused
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
-		if !get_tree().is_paused():
-			get_tree().set_pause(true)
-			set_visible(true)
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		if !self.visible:
+			#get_tree().set_pause(true)
+			MenuMenu.set_visible(true)
+			#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
-			get_tree().set_pause(false)
-			set_visible(false)
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			#get_tree().set_pause(false)
+			MenuMenu.set_visible(false)
+			#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		
 	
 func focus_entered():
-	SoundManager.PlayMouseEffect()
+	pass
+	#SoundManager.PlayMouseEffect()
 	
 func _on_Start_button_down():
-	SoundManager.PlayButtonPressUp()
+	#SoundManager.PlayButtonPressUp()
 	AnimPlayer.play("loading")
 
 func _on_Options_button_down():
-	SoundManager.PlayButtonPressUp()
+	#SoundManager.PlayButtonPressUp()
 	MenuMenu.hide()
 	OptionMenu.show()
 
 func _on_End_button_down():
-	SoundManager.PlayFX(SoundManager.ErrorSound)
+	pass
+	#SoundManager.PlayFX(SoundManager.ErrorSound)
 
 	QuitConfirm.popup_centered_ratio(.2)
 	var OkayButton = QuitConfirm.get_ok()
@@ -48,24 +49,24 @@ func _on_End_button_down():
 func _on_AcceptDialog_confirmed():
 	QuitConfirm.hide()
 	_on_resume_pressed()
-	SceneManager.ChangeScene("res://Menu/MainMenu/MainMenu2.tscn")
+	#SceneManager.ChangeScene("res://Menu/MainMenu/MainMenu2.tscn")
 
 func LoadNextScene():
-	SoundManager.TrackAnim.play("hide")
-	SceneManager.ChangeScene(NextScene)
+	pass
+	#SoundManager.TrackAnim.play("hide")
+	#SceneManager.ChangeScene(NextScene)
 
 func _on_back_pressed():
-	SoundManager.PlayButtonPressDown()
+	#SoundManager.PlayButtonPressDown()
 	MenuMenu.show()
 	OptionMenu.hide()
 
 func _on_resume_pressed():
-	SoundManager.TrackAnim.play("hide")
+	#SoundManager.TrackAnim.play("hide")
 	self.hide()
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().set_pause(false)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#get_tree().set_pause(false)
 
 func _on_Back_button_down():
-	SoundManager.PlayButtonPressDown()
+	#SoundManager.PlayButtonPressDown()
 	MenuMenu.show()
-	HowToPlay.hide()
