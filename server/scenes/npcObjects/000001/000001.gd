5904a13aacd42802500987b11ef21833fc8c2397
Christopher Nguyen <nguyech4@oregonstate.edu>
2023-12-21 14:03:30 -0800

    Update 000001.gd

--- server/scenes/npcObjects/000001/000001.gd
+++ server/scenes/npcObjects/000001/000001.gd
@@ -1,44 +1,9 @@
 extends Area2D
 var id = "000001"
 var title = "Kevin"
-var can_interact = false
-#const DIALOG = preload("res://scenes/userInerface/Dialog.tscn")
-var clicked = false
-
 
 func _physics_process(_delta):
 	pass
-	#$AnimatedSprite.play()
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass
-# warning-ignore:return_value_discarded
-	#Signals.connect("dialog_closed", self, "dialog_closed")
-"""
-func dialog_closed():
-	clicked = false
-	print("dialog should be closed and kevin should be clickable")
-
-func _on_Area2D_mouse_entered():
-	can_interact = true
-	print("can interact with kevin")
-
-
-func _on_Area2D_mouse_exited():
-	can_interact = false
-	print("cannot interact with kevin")
-
-func _on_Area2D_input_event(_viewport, event, _shape_idx):
-	#print(event)
-	if can_interact:
-		if event is InputEventMouseButton and clicked == false:
-			clicked = true
-			print("popup dialog for npc")
-			var dialog = DIALOG.instance()
-			Global.ui.add_child(dialog)
-			Global.movable = false
-			
-
-func _on_Area2D_area_entered(area):
-	print(area)# Replace with function body.
-"""
