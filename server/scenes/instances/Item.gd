80a0f40b3c549651cf67c7493d43af47fbadc5fa
ctopherngoc <ctopherngoc@gmail.com>
2024-03-08 12:43:45 -0800

    Update Item.gd
    Added script to collide with players

--- server/scenes/instances/Item.gd
+++ server/scenes/instances/Item.gd
@@ -13,17 +13,22 @@ func _process(delta):
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-func _on_Area2D_body_entered(body):
-	if player_owner:
-		if body.name == player_owner:
-			pass
-	else:
-		pass
-
 # player owner looting timer
 func _on_Timer_timeout():
+	print(self.name, " no owner")
 	player_owner = null
 
 # existance timer
 func _on_Timer2_timeout():
+	print(self.name, " discard item")
 	self.queue_free()
+
+
+func _on_Area2D_area_entered(area):
+	print("detect player")
+	if player_owner:
+		print(area.get_parent().name)
+		if area.get_parent().name == player_owner:
+			print("drop owner")
+	else:
+		pass
