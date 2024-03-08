085738bac3da7a8705e354a79fd8df9f8758f225
ctopherngoc <ctopherngoc@gmail.com>
2024-03-08 12:39:41 -0800

    Update 100002.gd
    removed old scripts

--- server/scenes/monsterObjects/100002/100002.gd
+++ server/scenes/monsterObjects/100002/100002.gd
@@ -38,32 +38,6 @@ func _process(delta):
 func _on_Timer_timeout():
 	move_state = floor(rand_range(0,3))
 
-
-#func npc_hit(dmg, player):
-#	current_hp -= dmg
-#	if str(player) in attackers.keys():
-#		attackers[str(player)] += dmg
-#	else:
-#		attackers[str(player)] = dmg
-#	# if dead change state and make it unhittable
-#	if current_hp <= 0:
-#		state = "Dead"
-#
-#		for attacker in attackers.keys():
-#			# if atacker logged in
-#			if attacker in ServerData.player_location.keys():
-#
-#				# if attacker in map
-#				if get_node("../../Players/%s" % attacker):
-#					var player_container = get_node("../../Players/%s" % attacker)
-#
-#					# xp = rounded (dmg done / max hp) * experience 
-#					player_container.experience(int(round((attackers[attacker] / max_hp) * experience)))
-#
-#		get_node("do_damage/CollisionShape2D").set_deferred("disabled", true)
-#
-#	print("monster: " + self.name + " health: " + str(current_hp))
-
 func touch_damage():
 	if $do_damage.get_overlapping_areas().size() > 0:
 		for player in $do_damage.get_overlapping_areas():
