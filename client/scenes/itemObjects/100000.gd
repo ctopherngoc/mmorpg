154979bac2a229cad659055c16db3048b27a25a7
Christopher Nguyen <52055142+ctopherngoc@users.noreply.github.com>
2024-03-15 14:38:19 -0700

    Create 100000.gd
    renamed coin.gd

---
+++ client/scenes/itemObjects/100000.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready():
+	pass
+	
+func _process(_delta):
+	"""
+	if player == true:
+		if (Input.is_action_pressed("loot")):
+			$AnimationPlayer.play("pickup")
+			print(str(value) + " coins")
+	"""
