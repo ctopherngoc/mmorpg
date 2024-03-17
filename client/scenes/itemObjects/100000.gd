ba929cc6c259f2f2fef1edaf9e72ca1ade6bafe8
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-03-16 17:38:33 -0700

    Update 100000.gd

--- client/scenes/itemObjects/100000.gd
+++ client/scenes/itemObjects/100000.gd
@@ -4,9 +4,4 @@ func _ready():
 	pass
 	
 func _process(_delta):
-	"""
-	if player == true:
-		if (Input.is_action_pressed("loot")):
-			$AnimationPlayer.play("pickup")
-			print(str(value) + " coins")
-	"""
+	pass
