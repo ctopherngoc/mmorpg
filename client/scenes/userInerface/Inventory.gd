c4ea608fd11a46d47bc1b71a61f2ff1f5fc569b6
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-04-27 23:09:06 -0700

    Update Inventory.gd
    updated inventory to show item icons and gold

--- client/scenes/userInerface/Inventory.gd
+++ client/scenes/userInerface/Inventory.gd
@@ -5,52 +5,75 @@ var inventory_slot = preload("res://scenes/userInerface/InventorySlot.tscn")
 onready var use_grid = $Background/M/V/TabContainer/Use/ScrollContainer/GridContainer
 onready var equip_grid = $Background/M/V/TabContainer/Equip/ScrollContainer/GridContainer
 onready var etc_grid = $Background/M/V/TabContainer/ETC/ScrollContainer/GridContainer
+onready var gold_label = $Background/M/V/ColorRect/HBoxContainer/goldLabel
 onready var inv_ref = {
+	"100000": 0,
 	"equipment": [],
 	"use": [],
 	"etc": [],
 }
+onready var stackable_tabs = ["etc", "use"]
+onready var item_path= "res://assets/itemSprites/"
 onready var max_slots = 32
 onready var inventory_tabs
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	inventory_tabs = inv_ref.keys()
 	for key in inventory_tabs:
-		var count = 0
-		while count < max_slots:
-			inv_ref[key].append(null)
-			count += 1
-		print(inv_ref[key])
+		if key != "100000":
+			var count = 0
+			while count < max_slots:
+				inv_ref[key].append(null)
+				count += 1
 	#var inventory_tabs = Global.player.inventory.keys()
 	#var inv_ref = Global.player.inventory
+	inv_ref["use"][0] = {'i': "300001", 'q': 5}
+	inv_ref["use"][1] = {'i': "300002", 'q': 500}
+	inv_ref["use"][6] = {'i': "300003", 'q': 420}
+	inv_ref["equipment"][1] = {'i': "500004"}
+	inv_ref["equipment"][6] = {'i': "500005"}
+	inv_ref["100000"] = 123456789
+	
 	
 	
 	# scroll through tabs (equip, use, etc) notmade yet
 	for tab in inventory_tabs:
-		print(tab)
-		var count = 0
-		# for item in each tab
-		while count < max_slots:
-			#for item in inv_ref[tab]:
-			var inv_slot_new = inventory_slot.instance()
-			# there is an item in data
-			#if item["itemID"] != null:
-			if inv_ref[tab][count] != null:
-				var item = inv_ref[tab][count]
-				var item_name = GameData.ItemTable[str(item["itemID"])]["itemName"]
-				#var item_name = GameData.ItemTable[str(item["itemID"])]["itemName"]
-				#var icon_texture = load()
-				
-				# if stackable
-				if item["amount"] != null:
-					pass
-					inv_slot_new.get_node("RichTextLabel").text = str(item["amount"])
-				#inv_slot_new.get_node("Icon").set_texture(icon_texture)
-			if tab == "equipment":
-				equip_grid.add_child(inv_slot_new, true)
-			elif tab == "use":
-				use_grid.add_child(inv_slot_new, true)
-			elif tab == "etc":
-				etc_grid.add_child(inv_slot_new, true)
-			count += 1
-		print(inv_ref[tab].size())
+		if tab == "100000":
+			gold_label.text = str(inv_ref[tab])
+		else:
+			var count = 0
+			# for item in each tab
+			while count < max_slots:
+				var inv_slot_new = inventory_slot.instance()
+				# there is an item in data
+				if inv_ref[tab][count] != null:
+					var item = inv_ref[tab][count]
+					var item_name = GameData.itemTable[str(item['i'])]["itemName"]
+					# if stackable exit number on iventory slot
+					if tab in stackable_tabs:
+						inv_slot_new.get_node("VBoxContainer/Label").text = str(item["q"])
+					if tab == "equipment":
+						var temp_item_path = item_path + "equipItems/" + inv_ref[tab][count]["i"] + ".png"
+						inv_slot_new.get_node("Icon").texture = load(temp_item_path)
+						equip_grid.add_child(inv_slot_new, true)
+					elif tab == "use":
+						var temp_item_path = item_path + "useItems/" + inv_ref[tab][count]["i"] + ".png"
+						inv_slot_new.get_node("Icon").texture = load(temp_item_path)
+						use_grid.add_child(inv_slot_new, true)
+					elif tab == "etc":
+						var temp_item_path = item_path + "etcItems/" + inv_ref[tab][count]["i"] + ".png"
+						inv_slot_new.get_node("Icon").texture = load(temp_item_path)
+						etc_grid.add_child(inv_slot_new, true)
+				else:
+					if tab == "equipment":
+						equip_grid.add_child(inv_slot_new, true)
+					elif tab == "use":
+						use_grid.add_child(inv_slot_new, true)
+					elif tab == "etc":
+						etc_grid.add_child(inv_slot_new, true)
+				count += 1
+
+func _input(event):
+	if event.is_action_pressed("inventory"):
+		self.visible = not self.visible
+		print("toggle inventory")
