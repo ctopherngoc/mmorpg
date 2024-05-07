ad4100785859fe934bdd3505d85ed7686ee06ece
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-05-07 16:49:45 -0700

    update print statement

--- client/scenes/userInerface/Inventory.gd
+++ client/scenes/userInerface/Inventory.gd
@@ -104,7 +104,7 @@ func update_inventory():
 					# get item node from node_list
 					var item = inv_ref[tab][count]
 					var item_node = node_list[tab][count]
-					
+					print("in update inventory, item: %s" % item)
 					var item_name = GameData.itemTable[str(item['id'])]["itemName"]
 					item_node.item_data["id"] = str(item['id'])
 					item_node.item_data["item"] = GameData.itemTable[str(item['id'])]["itemName"]
