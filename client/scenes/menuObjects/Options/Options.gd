ee7620343186481d4aba51586570162ae3aa5cf0
Christopher Nguyen <52055142+ctopherngoc@users.noreply.github.com>
2024-05-09 21:39:11 -0700

    Update Options.gd
    Added potential code for saving viewport and sound options

--- client/scenes/menuObjects/Options/Options.gd
+++ client/scenes/menuObjects/Options/Options.gd
@@ -1,3 +1,15 @@
 extends Control
 
-
+onready var sound_node = $PanelContainer/TabContainer/Sound
+onready var video_node = $PanelContainer/TabContainer/Video
+onready var option_path = "user://gameOption.dat"
+func _on_back_pressed():
+	var file = File.new()
+	file.open(option_path, file.WRITE)
+	var data = {}
+	file.store_var(data)
+	file.close()
+"""
+if Resolutions[r] == CurrentResolution:
+			ResolutionOptionButton._select_int(index)
+"""
