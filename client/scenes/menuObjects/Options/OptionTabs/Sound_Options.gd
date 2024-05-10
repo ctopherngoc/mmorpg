1ed04a101fe4f2a91a60f0b842919c64fa3e14e4
Christopher Nguyen <52055142+ctopherngoc@users.noreply.github.com>
2024-05-09 23:57:16 -0700

    Sound options save and loads

--- client/scenes/menuObjects/Options/OptionTabs/Sound_Options.gd
+++ client/scenes/menuObjects/Options/OptionTabs/Sound_Options.gd
@@ -3,28 +3,49 @@ extends HBoxContainer
 onready var SoundSlider = $Sliders/SoundSlider
 onready var EffectSlider = $Sliders/EffectsSlider
 onready var MasterSlider = $Sliders/MasterSlider
+onready var masterValue: float
+onready var musicValue: float
+onready var effectValue: float
 
-func _ready():
+func _ready() -> void:
 	pass
 	#SoundSlider.set_value(db2linear(SoundManager.GetBusVolume(SoundManager.BG_MUSIC_BUS)))
 	#EffectSlider.set_value(db2linear(SoundManager.GetBusVolume(SoundManager.MENU_EFFECT_BUS)))
 	#MasterSlider.set_value(db2linear(SoundManager.GetBusVolume(SoundManager.MASTER_BUS)))
 
 # warning-ignore:unused_argument
-func _on_SoundSlider_value_changed(value):
+func _on_SoundSlider_value_changed(value: float) -> void:
 	var sfx_index= AudioServer.get_bus_index("Music")
+	musicValue = value
 	AudioServer.set_bus_volume_db(sfx_index, linear2db(value))
 	#SoundManager.ChangeVolume(SoundManager.BG_MUSIC_BUS, linear2db(value))
 
 # warning-ignore:unused_argument
-func _on_EffectsSlider_value_changed(value):
+func _on_EffectsSlider_value_changed(value: float) -> void:
 	var sfx_index= AudioServer.get_bus_index("Effects")
+	effectValue = value
 	AudioServer.set_bus_volume_db(sfx_index, linear2db(value))
 	#SoundManager.ChangeVolume(SoundManager.MENU_EFFECT_BUS, linear2db(value))
 
 # warning-ignore:unused_argument
-func _on_MasterSlider_value_changed(value):
+func _on_MasterSlider_value_changed(value: float) -> void:
 	var sfx_index= AudioServer.get_bus_index("Master")
+	masterValue = value
 	AudioServer.set_bus_volume_db(sfx_index, linear2db(value))
 	#SoundManager.ChangeVolume(SoundManager.MASTER_BUS, linear2db(value))
 
+func save_sound_values() -> void:
+	effectValue = EffectSlider.value
+	masterValue = MasterSlider.value
+	musicValue = SoundSlider.value
+
+func set_sound_values() -> void:
+	var sfx_index= AudioServer.get_bus_index("Music")
+	SoundSlider.value = musicValue
+	AudioServer.set_bus_volume_db(sfx_index, linear2db(musicValue))
+	sfx_index = AudioServer.get_bus_index("Master")
+	MasterSlider.value = masterValue
+	AudioServer.set_bus_volume_db(sfx_index, linear2db(masterValue))
+	sfx_index = AudioServer.get_bus_index("Effects")
+	EffectSlider.value = effectValue
+	AudioServer.set_bus_volume_db(sfx_index, linear2db(effectValue))
--- client/scenes/menuObjects/Options/Options.gd
+++ client/scenes/menuObjects/Options/Options.gd
@@ -3,11 +3,39 @@ extends Control
 onready var sound_node = $PanelContainer/TabContainer/Sound
 onready var video_node = $PanelContainer/TabContainer/Video
 onready var option_path = "user://gameOption.dat"
-func _on_back_pressed():
+
+func _ready() -> void:
+	load_settings()
+
+func _on_back_pressed() -> void:
+	save_settings()
+
+func load_settings() -> void:
+	var save_file = File.new()
+	if not save_file.file_exists(option_path):
+		return
+	save_file.open(option_path, File.READ)
+	var settings_data = JSON.parse(save_file.get_as_text())
+	var sound_settings = settings_data.result["sound"]
+	sound_node.masterValue = float(sound_settings.master)
+	sound_node.effectValue = float(sound_settings.effect)
+	sound_node.musicValue = float(sound_settings.music)
+	sound_node.set_sound_values()
+	print("Loaded")
+	save_file.close()
+
+func save_settings() -> void:
+	sound_node.save_sound_values()
 	var file = File.new()
 	file.open(option_path, file.WRITE)
-	var data = {}
-	file.store_var(data)
+	var data ={"sound": {
+		"master": sound_node.masterValue,
+		"music": sound_node.musicValue,
+		"effect": sound_node.effectValue,
+		}
+	}
+	file.store_line(JSON.print(data, "\t"))
+	#file.store_var(data)
 	file.close()
 """
 if Resolutions[r] == CurrentResolution:
