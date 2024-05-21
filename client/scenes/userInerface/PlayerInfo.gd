fce415e5f2e6717022b9715a003d6e82bf64c1d0
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-05-20 21:17:59 -0700

    Updated references

--- client/scenes/userInerface/PlayerInfo.gd
+++ client/scenes/userInerface/PlayerInfo.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 # warning-ignore:return_value_discarded
 	Signals.connect("update_health", self, "update_health")
 # warning-ignore:return_value_discarded
@@ -15,30 +15,30 @@ func _ready():
 	load_info()
 
 #have to round to whole numbers
-func update_health():
-	$health/Label.text = str(Global.player.stats.base.health) + "/" + str(Global.player.stats.base.maxHealth)
-	$health/TextureProgress.value = (float(Global.player.stats.base.health) / float(Global.player.stats.base.maxHealth))* 100
+func update_health() -> void:
+	$Control/HBoxContainer/fillBars/hpmpBar/HealthVBar/Control/Number.text = str(Global.player.stats.base.health) + "/" + str(Global.player.stats.base.maxHealth)
+	$Control/HBoxContainer/fillBars/hpmpBar/HealthVBar/Control/TextureProgress.value = (float(Global.player.stats.base.health) / float(Global.player.stats.base.maxHealth))* 100
 #have to round to whole numbers
-func update_mana():
-	$mana/Label.text = str(Global.player.stats.base.mana) + "/" + str(Global.player.stats.base.maxMana)
-	$mana/TextureProgress.value = float((Global.player.stats.base.mana) / float(Global.player.stats.base.maxMana)) * 100
+func update_mana() -> void:
+	$Control/HBoxContainer/fillBars/hpmpBar/MPVBar/Control/Number.text = str(Global.player.stats.base.mana) + "/" + str(Global.player.stats.base.maxMana)
+	$Control/HBoxContainer/fillBars/hpmpBar/MPVBar/Control/TextureProgress.value = float((Global.player.stats.base.mana) / float(Global.player.stats.base.maxMana)) * 100
 
-func update_level():
-	$level/Label.text = str(Global.player.stats.base.level)
+func update_level() -> void:
+	$Control/HBoxContainer/characterInfo/LVLHBox/Number.text = str(Global.player.stats.base.level)
 	#have to round to whole numbers
 # for xp to show up as percentage, maximum xp for level must be stored and transferred
-func update_exp():
+func update_exp() -> void:
 	var exp_percent = (float(Global.player.stats.base.experience) / float(GameData.experience_table[str(Global.player.stats.base.level)])) * 100
-	$exp/Label.text = str(Global.player.stats.base.experience) + "/" + str(GameData.experience_table[str(Global.player.stats.base.level)]) + ("%10.2f" % exp_percent) + "%"
-	$exp/TextureProgress.value = exp_percent
+	$Control/HBoxContainer/fillBars/EXPVBar/Control/Number.text = str(Global.player.stats.base.experience) + "/" + str(GameData.experience_table[str(Global.player.stats.base.level)]) + ("%10.2f" % exp_percent) + "%"
+	$Control/HBoxContainer/fillBars/EXPVBar/Control/TextureProgress.value = exp_percent
 
-func update_displayname():
-	$displayname/Label.text = str(Global.player.displayname)
+func update_displayname() -> void:
+	$Control/HBoxContainer/characterInfo/username.text = str(Global.player.displayname)
 	
-func update_job():
-	$job/Label.text = str(GameData.job_dict[str(Global.player.stats.base.job)])
+func update_job() -> void:
+	$Control/HBoxContainer/characterInfo/job.text = str(GameData.job_dict[str(Global.player.stats.base.job)])
 	
-func load_info():
+func load_info() -> void:
 	update_displayname()
 	update_health()
 	update_mana()
