780031437421a1f62d145002638a94e77194338d
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-05-21 17:52:25 -0700

    Update Global.gd

--- server/singletons/Global.gd
+++ server/singletons/Global.gd
@@ -433,7 +433,7 @@ func lootDrop(player: KinematicBody2D, item_container: KinematicBody2D) -> void:
 					# update map itemlist
 					ServerData.items[item_container.map].erase(item_container.name)
 					# add item to index and update client
-					inventory_ref[index] = {"id": str(item_container), "q": 1}
+					inventory_ref[index] = {"id": str(item_container.id), "q": 1}
 					server.send_loot_data(player.name, {"id": item_container.id})
 					server.update_player_stats(player)
 					# remove item node from map
