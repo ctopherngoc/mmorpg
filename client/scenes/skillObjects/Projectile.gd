e3caf542da32ad768e954d7abed1770adb311720
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-15 21:54:00 -0700

    add client projectible scene

---
+++ client/scenes/skillObjects/Projectile.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+var speed: int = 20
+var target
+
+onready var hithox = get_node("Hitbox")
+onready var rangebox = get_node("Range")
+	
+func _ready():
+	pass
+	
+func _physics_process(delta: float) -> void:
+	pass
