82ac281e09c56f2fb992da13d9d28f8a65693d7e
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-19 15:37:41 -0700

    Updated floating text with multi hit

--- client/scenes/userInerface/FloatingText.gd
+++ client/scenes/userInerface/FloatingText.gd
@@ -10,8 +10,8 @@ var max_size = Vector2(1,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	label.set_text(amount)
-	
+	#label.set_text(amount)
+	var start_position = self.position
 	match type:
 		"Heal":
 			label.set("custom_colors/font_color", Color("00ff15"))
@@ -20,7 +20,9 @@ func _ready():
 		"Critical":
 			max_size = Vector2(1.25,1.25)
 			label.set("custom_colors/font_color", Color("00ffd5"))
-	tween.interpolate_property(self, "scale", scale, max_size, 0.7, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	var end_postion = Vector2(0, -50) + start_position
+	tween.interpolate_property(self, "scale", scale, max_size, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	tween.interpolate_property(self, "position", start_position, end_postion, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	#tween.interpolate_property(self, "scale", max_size,  Vector2(0.1, 0.1), 0.7, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.3)
 	tween.start()
 
