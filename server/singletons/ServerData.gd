f059b153a07245a43c25a4243b2c736e64014cb8
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-19 15:39:39 -0700

    Fixed issue template did not have keybinds

--- server/singletons/ServerData.gd
+++ server/singletons/ServerData.gd
@@ -303,12 +303,12 @@ var static_data = {
 			"use": [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
 			}, #inventory
 		"keybind": {
-			'shift': null, 'ins': null, 'home': null, 'pgup': null, 'ctrl': null,  'del': null, 'end': null, 'pgdn': null,
+			'shift': null, 'ins': null, 'home': null, 'pgup': null, 'ctrl': "attack",  'del': null, 'end': null, 'pgdn': null,
 			'`': null, '1': null, '2': null, '3': null, '4': null, '5': null, '6': null, '7': null, '8': null, '9': null, '0': null, '-': null, '=': null,
 			'f1': null, 'f2': null, 'f3': null, 'f4': null, 'f5': null, 'f6': null, 'f7': null, 'f8': null, 'f9': null, 'f10': null, 'f11': null, 'f12': null,
-			'q': null, 'w': null, 'e': null, 'r': null, 't': null, 'y': null, 'u': null, 'i': null, 'o': null, 'p': null, '[': null, ']': null,
-			'a': null, 's': null, 'd': null, 'f': null, 'g': null, 'h': null, 'j': null, 'k': null, 'l': null, ';': null, "'": null,
-			'z': null, 'x': null, 'c': null, 'v': null, 'b': null, 'n': null, 'm': null, ',': null, '.': null, '/': null,
+			'q': null, 'w': null, 'e': null, 'r': null, 't': null, 'y': null, 'u': null, 'i': "inventory", 'o': null, 'p': null, '[': null, ']': null,
+			'a': null, 's': "stat", 'd': null, 'f': null, 'g': null, 'h': null, 'j': null, 'k': "skill", 'l': null, ';': null, "'": null,
+			'z': "loot", 'x': null, 'c': null, 'v': null, 'b': null, 'n': null, 'm': null, ',': null, '.': null, '/': null,
 			}, # keybind
 		}, #player_template
 	"starter_equips" : 
