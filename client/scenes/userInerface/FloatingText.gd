cbf32f95a68dfb2cca9b4601c9a5b7ab50a915ce
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-19 22:55:15 -0700

    Updated floating text

--- client/scenes/userInerface/FloatingText.gd
+++ client/scenes/userInerface/FloatingText.gd
@@ -10,21 +10,29 @@ var max_size = Vector2(1,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#label.set_text(amount)
-	var start_position = self.position
+	label.set_text(amount)
 	match type:
 		"Heal":
 			label.set("custom_colors/font_color", Color("00ff15"))
-		"Damage":
+			var start_position = self.position
+			var end_postion = Vector2(0, -50) + start_position
+			tween.interpolate_property(self, "scale", scale, max_size, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			tween.interpolate_property(self, "position", start_position, end_postion, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			tween.start()
+		"N":
 			label.set("custom_colors/font_color", Color("ffffff"))
-		"Critical":
+			yield(get_tree().create_timer(1.3),"timeout")
+			self.queue_free()
+		"C":
 			max_size = Vector2(1.25,1.25)
 			label.set("custom_colors/font_color", Color("00ffd5"))
-	var end_postion = Vector2(0, -50) + start_position
-	tween.interpolate_property(self, "scale", scale, max_size, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-	tween.interpolate_property(self, "position", start_position, end_postion, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-	#tween.interpolate_property(self, "scale", max_size,  Vector2(0.1, 0.1), 0.7, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.3)
-	tween.start()
+			yield(get_tree().create_timer(1.3),"timeout")
+			self.queue_free()
+		"M":
+			max_size = Vector2(1.25,1.25)
+			label.set("custom_colors/font_color", Color("f90000"))
+			yield(get_tree().create_timer(1.3),"timeout")
+			self.queue_free()
 
 func _on_Tween_tween_all_completed():
 	self.queue_free()
