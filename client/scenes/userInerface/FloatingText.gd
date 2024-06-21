fe0eeb7c995c5c159904f7a97d365f987d26c200
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-21 15:10:21 -0700

    Update FloatingText.gd

--- client/scenes/userInerface/FloatingText.gd
+++ client/scenes/userInerface/FloatingText.gd
@@ -12,28 +12,32 @@ var max_size = Vector2(1,1)
 func _ready():
 	label.set_text(amount)
 	match type:
-		"Heal":
+		"PH":
 			label.set("custom_colors/font_color", Color("00ff15"))
-			var start_position = self.position
-			var end_postion = Vector2(0, -50) + start_position
-			tween.interpolate_property(self, "scale", scale, max_size, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-			tween.interpolate_property(self, "position", start_position, end_postion, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-			tween.start()
+			yield(get_tree().create_timer(1.3),"timeout")
+			self.queue_free()
+#			var start_position = self.position
+#			var end_postion = Vector2(0, -50) + start_position
+#			tween.interpolate_property(self, "scale", scale, max_size, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+#			tween.interpolate_property(self, "position", start_position, end_postion, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+#			tween.start()
 		"N":
 			label.set("custom_colors/font_color", Color("ffffff"))
 			yield(get_tree().create_timer(1.3),"timeout")
 			self.queue_free()
 		"C":
-			max_size = Vector2(1.25,1.25)
 			label.set("custom_colors/font_color", Color("00ffd5"))
 			yield(get_tree().create_timer(1.3),"timeout")
 			self.queue_free()
 		"M":
-			max_size = Vector2(1.25,1.25)
+			label.set("custom_colors/font_color", Color("9700ff"))
+			yield(get_tree().create_timer(1.3),"timeout")
+			self.queue_free()
+		"PN":
 			label.set("custom_colors/font_color", Color("f90000"))
 			yield(get_tree().create_timer(1.3),"timeout")
 			self.queue_free()
-
+			
 func _on_Tween_tween_all_completed():
 	self.queue_free()
 
