77245dd49e0bb3141ebc0e3e128fa5c13ee995a4
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-23 12:07:58 -0700

    Update monsterTemplate.gd

--- server/scenes/monsterObjects/monsterTemplate/monsterTemplate.gd
+++ server/scenes/monsterObjects/monsterTemplate/monsterTemplate.gd
@@ -18,11 +18,15 @@ var attackers = {}
 onready var hit_timer = $Timer2
 onready var target_node
 onready var target_position
+onready var sprite_scale = Vector2(0.4, 0.4)
 
 func _ready():
 	stats = ServerData.monsterTable[self.id].duplicate(true)
+	self.scale = sprite_scale
 	
 func _process(delta):
+	if self.scale != sprite_scale:
+		self.scale = sprite_scale
 	touch_damage()
 	
 	if state != "hit":
