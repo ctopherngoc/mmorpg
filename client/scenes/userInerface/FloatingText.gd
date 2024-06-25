ae9701fbdf212bb522384f32a125f2d6412ebc45
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-24 18:34:16 -0700

    fixed issue healing did not queue free

--- client/scenes/userInerface/FloatingText.gd
+++ client/scenes/userInerface/FloatingText.gd
@@ -4,6 +4,7 @@ onready var label = $Label
 onready var tween = $Tween
 var amount: String
 var type: String
+onready var timer = $Timer
 
 var velocity = Vector2(1,1)
 var max_size = Vector2(1,1)
@@ -14,8 +15,8 @@ func _ready():
 	match type:
 		"PH":
 			label.set("custom_colors/font_color", Color("00ff15"))
-			yield(get_tree().create_timer(1.3),"timeout")
-			self.queue_free()
+			#yield(get_tree().create_timer(1.0),"timeout")
+			#self.queue_free()
 #			var start_position = self.position
 #			var end_postion = Vector2(0, -50) + start_position
 #			tween.interpolate_property(self, "scale", scale, max_size, 1.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
@@ -23,24 +24,28 @@ func _ready():
 #			tween.start()
 		"N":
 			label.set("custom_colors/font_color", Color("ffffff"))
-			yield(get_tree().create_timer(1.3),"timeout")
-			self.queue_free()
+			#yield(get_tree().create_timer(1.0),"timeout")
+			#self.queue_free()
 		"C":
 			label.set("custom_colors/font_color", Color("00ffd5"))
-			yield(get_tree().create_timer(1.3),"timeout")
-			self.queue_free()
+			#yield(get_tree().create_timer(1.0),"timeout")
+			#self.queue_free()
 		"M":
 			label.set("custom_colors/font_color", Color("9700ff"))
-			yield(get_tree().create_timer(1.3),"timeout")
-			self.queue_free()
+			#yield(get_tree().create_timer(1.0),"timeout")
+			#self.queue_free()
 		"PN":
 			label.set("custom_colors/font_color", Color("f90000"))
-			yield(get_tree().create_timer(1.3),"timeout")
-			self.queue_free()
-			
+			#yield(get_tree().create_timer(1.0),"timeout")
+			#self.queue_free()
+	timer.start()
+	
 func _on_Tween_tween_all_completed():
 	self.queue_free()
 
-
 func _process(delta):
 	position -= velocity * delta
+
+
+func _on_Timer_timeout():
+	self.queue_free()
