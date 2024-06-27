989e43fe6d1b3c60ee3f0638f12db9d48734a037
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-26 22:13:00 -0700

    Update DebuggerWindow.gd

--- client/scenes/userInerface/DebugWindow/DebuggerWindow.gd
+++ client/scenes/userInerface/DebugWindow/DebuggerWindow.gd
@@ -7,4 +7,10 @@ func _ready():
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	if Global.in_game:
-		label.text = str(Global.player.stats.base.movementSpeed + Global.player.stats.equipment.movementSpeed + Global.player.stats.buff.movementSpeed)
+		label.text = "movementSpeed: " + str(Global.player.stats.base.movementSpeed + Global.player.stats.equipment.movementSpeed + Global.player.stats.buff.movementSpeed)
+		label.text += "\nStrength: " + str(Global.player.stats.base.strength + Global.player.stats.equipment.strength + Global.player.stats.buff.strength)
+		label.text += "\nDexterity: " + str(Global.player.stats.base.dexterity + Global.player.stats.equipment.dexterity + Global.player.stats.buff.dexterity)
+		label.text += "\nWisdom: " + str(Global.player.stats.base.wisdom + Global.player.stats.equipment.wisdom + Global.player.stats.buff.wisdom)
+		label.text += "\nLuck: " + str(Global.player.stats.base.luck + Global.player.stats.equipment.luck + Global.player.stats.buff.luck)
+		label.text += "\nmagicDefense: " + str(Global.player.stats.base.magicDefense + Global.player.stats.equipment.magicDefense + Global.player.stats.buff.magicDefense)
+		label.text += "\nDefense: " + str(Global.player.stats.base.defense + Global.player.stats.equipment.defense + Global.player.stats.buff.defense)
