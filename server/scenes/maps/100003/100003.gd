4237633e1d4d54cdab2ed39d33a75a00b4f94751
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-30 14:55:21 -0700

    Update 100003.gd

--- server/scenes/maps/100003/100003.gd
+++ server/scenes/maps/100003/100003.gd
@@ -4,27 +4,15 @@ var map_name = "Grassy Road 3"
 
 var enemy_id_counter = 0
 var enemy_maximum = 19
-var spawn_position = Vector2(350, -462)
+var spawn_position = Vector2(350, -550)
 
-var green_guy = preload("res://scenes/monsterObjects/100001/100001.tscn")
-var blue_guy = preload("res://scenes/monsterObjects/100002/100002.tscn")
-var chick = preload("res://scenes/monsterObjects/100003/100003.tscn")
+var bull_rooster = preload("res://scenes/monsterObjects/100010/100010.tscn")
 
-var enemy_types = [green_guy, green_guy, blue_guy, blue_guy,
-					chick, chick, blue_guy, blue_guy,
-					chick, chick, chick, blue_guy,
-					blue_guy, chick, green_guy, green_guy,
-					green_guy, chick, green_guy
-]					
+var enemy_types = [bull_rooster,]
 
-var enemy_spawn_points = [Vector2(367, -93), Vector2(631, -93), Vector2(355, -433), Vector2(631, -433),
-							Vector2(407, -414), Vector2(564, -414), Vector2(856, -414), Vector2(987, -414),
-							Vector2(1109, -414), Vector2(1223, -414), Vector2(1392, -414), Vector2(1587, -414),
-							Vector2(1758, -414), Vector2(550, -71), Vector2(878, -71), Vector2(1089, -71),
-							Vector2(1247, -414), Vector2(1503, -71), Vector2(1740, -71),
-]
+var enemy_spawn_points = [Vector2(758, -614),]
 
-var open_locations = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+var open_locations = [0]
 
 var occupied_locations = {}
 var enemy_list = {}
@@ -32,39 +20,39 @@ var players = []
 onready var player_ysort = $YSort/Players
 
 func _ready():
-	pass
-	#var timer = Timer.new()
-	#timer.wait_time = 3
-	#timer.autostart = true
-	#timer.connect("timeout", self, "SpawnEnemy")
-	#self.add_child(timer)
+	var timer = Timer.new()
+	timer.wait_time = 3
+	timer.autostart = true
+	timer.connect("timeout", self, "SpawnEnemy")
+	self.add_child(timer)
 
 var counter = 0
 
 func _process(_delta):
 	if enemy_list.size() == 0:
 		pass
-#	else:
-#		for monster_id in enemy_list.keys():
-#			if enemy_list[monster_id]['EnemyState'] != "Dead":
-#				var monster_container = get_node("YSort/Monsters/%s" % str(monster_id))
-#				enemy_list[monster_id]['EnemyLocation'] = monster_container.position
-#				enemy_list[monster_id]['EnemyHealth'] = monster_container.stats.currentHP
-#				enemy_list[monster_id]['EnemyState'] = monster_container.state
-#				enemy_list[monster_id]['MissCounter'] = monster_container.miss_counter
-#				if monster_container.damage_taken.size() > 0:
-#					counter = 0
-#					enemy_list[monster_id]['DamageList'] = monster_container.damage_taken.duplicate(true)
-#					monster_container.damage_taken.clear()
-#				else:
-#					if counter < 5:
-#						counter += 1
-#					else:
-#						counter = 0
-#						enemy_list[monster_id]['DamageList'].clear()
+	else:
+		for monster_id in enemy_list.keys():
+			if enemy_list[monster_id]['EnemyState'] != "Dead":
+				var monster_container = get_node("YSort/Monsters/%s" % str(monster_id))
+				enemy_list[monster_id]['EnemyLocation'] = monster_container.position
+				enemy_list[monster_id]['EnemyHealth'] = monster_container.stats.currentHP
+				enemy_list[monster_id]['EnemyState'] = monster_container.state
+				enemy_list[monster_id]['MissCounter'] = monster_container.miss_counter
+				enemy_list[monster_id]['Direction'] = monster_container.direction.x
+				if monster_container.damage_taken.size() > 0:
+					counter = 0
+					enemy_list[monster_id]['DamageList'] = monster_container.damage_taken.duplicate(true)
+					monster_container.damage_taken.clear()
+				else:
+					if counter < 5:
+						counter += 1
+					else:
+						counter = 0
+						enemy_list[monster_id]['DamageList'].clear()
 	UpdateItemStateList()
 	UpdateProjectileStateList()
-#	UpdateMonsterList()
+	UpdateMonsterList()
 	if player_ysort.get_children() != players:
 		players = player_ysort.get_children()
 # after timer function called
@@ -84,6 +72,7 @@ func SpawnEnemy():
 				########################################### 
 				# spawns server enemy in map
 				var new_enemy = enemy_types[i].instance()
+				new_enemy.id = new_enemy.name
 				new_enemy.map_id = map_id
 				new_enemy.position = location
 				new_enemy.name = str(i)
@@ -107,7 +96,6 @@ func UpdateItemStateList() -> void:
 	ServerData.items.keys() are item nodes name. Unique 6 len string of Uppercase Chars and Ints
 	"""
 	if  get_node("YSort/Items").get_child_count() > 0:
-		print("yes")
 		var _index  = 0
 		for item in get_node("YSort/Items").get_children():
 			Global.add_item_to_world_state(item, self.name)
