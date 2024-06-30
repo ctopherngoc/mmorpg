0cd9c4e65e75db104780066d3ae6d83daac1c7e5
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-30 14:55:07 -0700

    Update 100002.gd

--- client/scenes/monsterObjects/100002/100002.gd
+++ client/scenes/monsterObjects/100002/100002.gd
@@ -20,64 +20,24 @@ func _ready():
 	pass
 	
 # new functions
-func move(new_position):
+func move(new_position, state, direction):
 	if despawn == 1:
 		var curr_position = self.get_position()
 		#turn right
 		if new_position.x > curr_position.x and abs(new_position.x - curr_position.x) > 0.2:
-			$Sprite.scale.x = xScale * -1
+			if direction == 1:
+				$Sprite.scale.x = xScale * -1
 			animation_control('walk')
 		#turn left
 		elif new_position.x < curr_position.x and abs(new_position.x - curr_position.x) > 0.2:
-			$Sprite.scale.x = xScale
+			if direction == -1:
+				$Sprite.scale.x = xScale
 			animation_control('walk')
 		else:
 			animation_control('idle')
 		set_position(new_position)
 
-#func health(health):
-#	if health < current_hp:
-#		var damage_took = str(current_hp - health)
-#		print("%s took %s damage" % [self.name, damage_took])
-#		AudioControl.play_audio("squish")
-#		var damage_text = floating_text.instance()
-#		damage_text.type = "Damage"
-#		damage_text.amount = damage_took
-#		add_child(damage_text)
-#		current_hp = health
-#		health_bar_update()
-
-#	elif health > current_hp:
-#		var damage_heal = str(current_hp - health)
-#		print("%s healed %s health" % [self.name, damage_heal])
-#		#AudioControl.play_audio("squish")
-#		var heal_text = floating_text.instance()
-#		heal_text.type = "Heal"
-#		heal_text.amount = damage_heal
-#		add_child(heal_text)
-#		current_hp -= health
-#		health_bar_update()
-#
-#func damage_taken(health, damage_array: Array) -> void:
-##	if not damage_array.empty():
-##		print("damage arr ", damage_array)
-##		print("health: ", health)
-#	current_hp = health
-#	var damage_text = floating_text.instance()
-#	damage_text.type = "Damage"
-#	add_child(damage_text)
-#	for damage in damage_array:
-#		print(str(self.name) + " took " + str(damage) + " damage")
-#		#damage_text.amount = damage
-#		damage_text.label.text += "%s\n" % str(damage)
-#		#current_hp -= damage
-#		health_bar_update()
-#		yield(get_tree().create_timer(0.1),"timeout")
-
 func damage_taken(health, damage_array: Array) -> void:
-#	if not damage_array.empty():
-#		print("damage arr ", damage_array)
-#		print("health: ", health)
 	current_hp = health
 	var lines = 0
 	for damage in damage_array:
