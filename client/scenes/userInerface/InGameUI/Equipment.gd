a01e5d19a762a9dac7cad38338440d13f3695ede
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-06-30 18:49:07 -0700

    Update Equipment.gd

--- client/scenes/userInerface/InGameUI/Equipment.gd
+++ client/scenes/userInerface/InGameUI/Equipment.gd
@@ -86,46 +86,6 @@ func populate_equipment():
 				equipment_tab[slot].item_data["id"] = equipment_ref[slot].id
 				equipment_tab[slot].item_data["item"] = equipment_ref[slot]
 
-#
-#func update_equipment():
-#	# inventory_tabs can be changed to global.player.inventory
-#	inv_ref = Global.player.inventory
-#	for tab in inv_ref:
-#		if tab == "100000":
-#			gold_label.text = str(inv_ref[tab])
-#		else:
-#			var count = 0
-#			max_slots = inv_ref[tab].size()
-#			# for item in each tab
-#			while count < max_slots:
-#
-#				if inv_ref[tab][count] != null:
-#					# get item node from node_list
-#					var item = inv_ref[tab][count]
-#					var item_node = node_list[tab][count]
-#					#print("in update inventory, item: %s" % item)
-#					var _item_name = GameData.itemTable[str(item['id'])]["itemName"]
-#					item_node.item_data["id"] = str(item['id'])
-#					item_node.item_data["item"] = GameData.itemTable[str(item['id'])]["itemName"]
-#					# if stackable exit number on iventory slot
-#					if tab in stackable_tabs:
-#						item_node.label.text = str(item["q"])
-#						item_node.item_data["q"]= item["q"]
-#					if tab == "equipment":
-#						var temp_item_path = item_path + "equipItems/" + inv_ref[tab][count]["id"] + ".png"
-#						item_node.icon.texture = load(temp_item_path)
-#					elif tab == "use":
-#						var temp_item_path = item_path + "useItems/" + inv_ref[tab][count]["id"] + ".png"
-#						item_node.icon.texture = load(temp_item_path)
-#					elif tab == "etc":
-#						var temp_item_path = item_path + "etcItems/" + inv_ref[tab][count]["id"] + ".png"
-#						item_node.icon.texture = load(temp_item_path)
-#				else:
-#					var item_node = node_list[tab][count]
-#					item_node.item_data = {"id": null,"item": null, "q": null}
-#					item_node.icon.set_texture(null)
-#				count += 1
-
 func _on_Header_gui_input(event):
 	if event is InputEventMouseButton:
 		# left mouse button
@@ -138,33 +98,6 @@ func _on_Header_gui_input(event):
 	if event is InputEventMouseMotion and drag_position:
 		rect_global_position = get_global_mouse_position() - drag_position
 			
-
-"""
-func update inventory
-
-Required to add rpc calls to server to swap inventory data.
-Server remove func to validate item move request -> 
-update server char inventory data -> client remote func to update character data ->
- update inventory window icons (similar to health hud)
-"""
-
-#func test_setup():
-#	inventory_tabs = inv_ref.keys()
-#	for key in inventory_tabs:
-#		if key != "100000":
-#			var count = 0
-#			while count < max_slots:
-#				inv_ref[key].append(null)
-#				count += 1
-#	#var inventory_tabs = Global.player.inventory.keys()
-#	#var inv_ref = Global.player.inventory
-#	inv_ref["use"][0] = {'id': "300001", 'q': 5}
-#	inv_ref["use"][1] = {'id': "300002", 'q': 500}
-#	inv_ref["use"][6] = {'id': "300003", 'q': 420}
-#	inv_ref["equipment"][1] = {'id': "500004"}
-#	inv_ref["equipment"][6] = {'id': "500005"}
-#	inv_ref["100000"] = 123456789
-
 
 func _on_TabContainer_tab_selected(_tab):
 	AudioControl.play_audio("menuClick")
