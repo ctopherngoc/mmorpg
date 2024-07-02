65d707f307c41cdcf81e9291e386444166a7b6af
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-07-01 22:47:16 -0700

    Added skill info window

--- client/scenes/userInerface/InGameUI/SkillIcon.gd
+++ client/scenes/userInerface/InGameUI/SkillIcon.gd
@@ -1,6 +1,8 @@
 extends TextureRect
 
 onready var type = "skill"
+onready var skill_info  = preload("res://scenes/userInerface/ItemInfo/SkillInfo.tscn")
+onready var skill_data
 
 func _ready():
 	pass
@@ -28,3 +30,23 @@ func get_drag_data(_pos):
 		set_drag_preview(control)
 		
 		return data
+
+func skill_info_free():
+	for node in self.get_children():
+		if "SkillInfo" in node.name:
+			node.queue_free()
+
+func _on_Icon_mouse_entered():
+	var skill_node = self.get_parent().get_parent().get_parent()
+	var skill_tip = skill_info.instance()
+	skill_tip.skill_id = skill_data.id
+	var skills_origin = get_node("/root/GameWorld/UI/Control/Skills").rect_global_position
+	skill_tip.rect_position.x = skills_origin.x - (skill_tip.rect_size.x *  skill_tip.rect_scale.x) + 5
+	skill_tip.rect_position.y = skills_origin.y
+	add_child(skill_tip)
+	yield(get_tree().create_timer(0.35), "timeout")
+	if has_node("SkillInfo"): # and get_node("SkillInfo").valid:
+		get_node("SkillInfo").show()
+
+func _on_Icon_mouse_exited():
+	skill_info_free()
