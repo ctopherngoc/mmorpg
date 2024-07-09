5698876b796d6de9a8df859575820a4a8b7cd08b
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-07-09 15:52:08 -0700

    Separated player and npc chat

--- client/scenes/userInerface/NPCChatBox.gd
+++ client/scenes/userInerface/NPCChatBox.gd
@@ -2,24 +2,13 @@ extends MarginContainer
 
 onready var label = $MarginContainer/VBoxContainer/message
 onready var timer = $Timer
-onready var  display_name: String = ""
 const MIN_SIZE = Vector2(100, 36)
-
 
 func _ready():
 	self.rect_size = self.MIN_SIZE
 	self.visible = false
 
 func display_text(text_to_display: String):
-	if text_to_display == "":
-		pass
-	else:
-		var text = text_to_display
-		label.text = display_name + ": " + text_to_display
-		timer.start()
-		self.visible = true
-		
-func npc_display_text(text_to_display: String):
 	var text = text_to_display
 	label.text = text_to_display
 	timer.start()
@@ -28,4 +17,5 @@ func npc_display_text(text_to_display: String):
 func _on_Timer_timeout():
 	self.visible = false
 	label.text = ""
-	self.rect_size = self.MIN_SIZE
+	#self.rect_size = self.MIN_SIZE
+	#self.rect_position.y = -104
