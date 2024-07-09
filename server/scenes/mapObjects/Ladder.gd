93eb591683c3499ed83085c087f87e557c5311f7
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-07-08 17:29:02 -0700

    Update Ladder.gd

--- server/scenes/mapObjects/Ladder.gd
+++ server/scenes/mapObjects/Ladder.gd
@@ -7,7 +7,6 @@ func _on_Ladder_area_entered(area):
 			Global.send_climb_data(int(self.name), 1)
 			parent.can_climb = true
 
-
 func _on_Ladder_area_exited(area):
 	if area.is_in_group("climber"):
 		var parent = area.get_parent()
