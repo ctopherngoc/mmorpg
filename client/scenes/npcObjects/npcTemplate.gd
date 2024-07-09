5e884e08427cf04009448b9b94765883048fdb7a
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-07-09 15:53:35 -0700

    Update npcTemplate.gd

--- client/scenes/npcObjects/npcTemplate.gd
+++ client/scenes/npcObjects/npcTemplate.gd
@@ -13,13 +13,20 @@ var clicked = false
 func _ready():
 	pass
 
-
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if can_interact:
 		if event is InputEventMouseButton and clicked == false:
 			clicked = true
 			print("popup dialog for npc")
-#			var dialog = DIALOG.instance()
+#			var dialog = DIALOG.instance()s
 #			Global.ui.add_child(dialog)
 #			Global.movable = false
 			
+
+func move(location):
+	if location.x < position.x:
+		self.flip_h == true
+	elif location.x > position.x:
+		self.flip_h = false
+	else:
+		pass
