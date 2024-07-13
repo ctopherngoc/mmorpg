1d3a655ea7d41a8a587de84190dabd6294396d06
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-07-12 18:08:41 -0700

    fixed npc wrong id

--- client/scenes/npcObjects/100001/100001.gd
+++ client/scenes/npcObjects/100001/100001.gd
@@ -1,56 +1,54 @@
 extends Sprite
 
-onready var id = "100000"
+onready var id = "100001"
 onready var data = GameData.npcTable[self.id]
-onready var sprite = preload("res://assets/npcSprites/100000/100000.png")
+onready var sprite = preload("res://assets/npcSprites/100001/100001.png")
+onready var spriteW = preload("res://assets/npcSprites/100001/100001w.png")
 onready var label = $Label
 onready var anim = $AnimationPlayer
 onready var dialog_index = 0
 onready var dialog_bubble = $ChatBox
 onready var dialog_timer = $DialogTimer
-onready var test_questTable
-onready var test_npcTable
-
-onready var dialog_box = preload("res://scenes/userInerface/Dialog.tscn")
 
 onready var can_interact = true
 var clicked = false
 
 func _ready() -> void:
-	Signals.connect("npc_click_false", self, "npc_click_false")
 	label.text = data.name
 	dialog_timer.start()
-	var data_file = File.new()
-	data_file.open("res://data/GameDataTable.json", File.READ)
-	var gamedata_json = JSON.parse(data_file.get_as_text())
-	test_questTable = gamedata_json.result["QuestTable"]
-	test_npcTable = gamedata_json.result["NPCTable"]
-	data_file.close()
 	
 func _on_Area2D_input_event(viewport, event, shape_idx) -> void:
-	if can_interact and not clicked:
+	if can_interact:
 		if event is InputEventMouseButton:
-			if event.button_index == BUTTON_LEFT and event.pressed:
-				print("popup dialog for npc")
-				clicked = true
-				var dialog = dialog_box.instance()
-				dialog.npc_id = "100001"
-				dialog.chatDialog = test_npcTable[self.name].chatDialog
-				self.add_child(dialog)
+			#clicked = true
+			print("popup dialog for npc")
+#			var dialog = DIALOG.instance()
+#			Global.ui.add_child(dialog)
+#			Global.movable = false
+			
+
+func move(location) -> void:
+	#print(location)
+	if location.x < position.x:
+		self.flip_h = true
+		self.texture = spriteW
+		self.set_position(location)
+	elif location.x > position.x:
+		self.flip_h = false
+		self.texture = spriteW
+		self.set_position(location)
+	else:
+		self.texture = sprite
 
 # keeps bubble open
 func _on_DialogTimer_timeout():
-	pass
-#	print(dialog_bubble.rect_position)
-#	dialog_bubble.display_text(data.chatDialog[dialog_index])
-#	if dialog_index + 1 == data.chatDialog.size():
-#		dialog_index = 0
-#	else:
-#		dialog_index += 1
+	print(dialog_bubble.rect_position)
+	dialog_bubble.display_text(data.bubbleDialog[dialog_index])
+	if dialog_index + 1 == data.bubbleDialog.size():
+		dialog_index = 0
+	else:
+		dialog_index += 1
 
 func _on_Area2D_mouse_entered():
 	print("dectect mouse")
-	
-func npc_click_false() -> void:
-	self.clicked = false
 
