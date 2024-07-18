5be14dfdf5cdb25c33250d9ef9ab27669a8cbdc9
ctopherngoc <52055142+ctopherngoc@users.noreply.github.com>
2024-07-17 17:23:51 -0700

    update npc with quest bubble

--- client/scenes/npcObjects/100000/100000.gd
+++ client/scenes/npcObjects/100000/100000.gd
@@ -3,36 +3,32 @@ extends Sprite
 onready var id = "100000"
 onready var data = GameData.npcTable[self.id]
 onready var sprite = preload("res://assets/npcSprites/100000/100000.png")
+onready var avaliable_texture = preload("res://assets/npcSprites/npcBubbleSprites/QuestBubbleSprite.png")
+onready var active_texture = preload("res://assets/npcSprites/npcBubbleSprites/ActiveQuestBubbleSprite.png")
 onready var label = $Label
 onready var anim = $AnimationPlayer
 onready var dialog_index = 0
 onready var dialog_bubble = $ChatBox
 onready var dialog_timer = $DialogTimer
+onready var quest_bubble = $QuestBubble
 
 onready var dialog_box = preload("res://scenes/userInerface/Dialog.tscn")
 
-#onready var can_interact = true
-#var clicked = false
-
 func _ready() -> void:
+	Signals.connect("update_quest_log", self, "update_bubble")
 	label.text = data.name
 	dialog_timer.start()
+	update_bubble()
 
 func _on_Area2D_input_event(viewport, event, shape_idx) -> void:
 	#if can_interact and not clicked:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			print("popup dialog for npc")
-			#clicked = true
-#			var dialog = dialog_box.instance()
-#			dialog.npc_id = self.id
-#			dialog.chatDialog = data.chatDialog
-#			self.add_child(dialog)
 			Signals.emit_signal("toggle_dialog", self.id)
 
 # keeps bubble open
 func _on_DialogTimer_timeout():
-	#print(dialog_bubble.rect_position)
 	dialog_bubble.display_text(data.bubbleDialog[dialog_index])
 	if dialog_index + 1 == data.bubbleDialog.size():
 		dialog_index = 0
@@ -42,3 +38,27 @@ func _on_DialogTimer_timeout():
 func _on_Area2D_mouse_entered():
 	print("dectect mouse")
 
+func update_bubble():
+	# 1 = avaliable 2 = active
+	var bubble_status = 0
+	
+	# quest index tracker
+	var quest_id = 0
+	for quest in Global.quest_data:
+		if quest[0] == -1 and not bubble_status:
+			bubble_status = 1
+		elif quest[0] == 0 and quest[0] != 9:
+			bubble_status = 2
+			break
+		quest_id += 1
+		
+	if bubble_status == 1:
+		quest_bubble.texture = avaliable_texture
+		if not quest_bubble.visible:
+			avaliable_texture.visible = true
+	elif bubble_status == 2:
+		quest_bubble.texture = active_texture
+		if not quest_bubble.visible:
+			avaliable_texture.visible = true
+	else:
+		quest_bubble.visible = false
